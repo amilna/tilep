a0583aa79d31cca8a8f60e597c2491442688a489
Satrio Arditama <iyo@amilna.com>
2014-11-04 09:08:50 +0700

    update tilep

--- index.php
+++ index.php
@@ -5,10 +5,11 @@ $zoom = $_REQUEST['z'];
 $xtile = $_REQUEST['x'];
 $ytile = $_REQUEST['y'];
 $type = $_REQUEST['type'];
+$clear = $_REQUEST['clear'];
 
 include('./tilep.php');
 
 $tilep = new tilep();
-$tilep->createTile($xml,$zoom,$xtile,$ytile,$type = false);
+$tilep->createTile($xml,$zoom,$xtile,$ytile,$type,$clear);
 
 ?>
--- tilep.php
+++ tilep.php
@@ -10,48 +10,55 @@ class tilep {
 		return array($lon_deg,$lat_deg);
 	}
 
-	public function createTile($xml,$zoom,$xtile,$ytile,$type = false)
-	{
-		
+	public function createTile($xml,$zoom,$xtile,$ytile,$type = false,$clear = false)
+	{						
 		$dir = $xml."/".$zoom."/".$xtile;
-		$bfile = $dir."/".$ytile;
-		$img = $bfile.".png";
-			
-		$width = 256; 
-		$height = 256;
-		$tile_size = 256;
-
-		$xtile_s = ($xtile * $tile_size - $width/2) / $tile_size;
-		$ytile_s = ($ytile * $tile_size - $height/2) / $tile_size;
-		$xtile_e = ($xtile * $tile_size + $width/2) / $tile_size;
-		$ytile_e = ($ytile * $tile_size + $height/2) / $tile_size; 
-
-		$s0 = $this->getLonLat($xtile_s, $ytile_s, $zoom);
-		$e0 = $this->getLonLat($xtile_e, $ytile_e, $zoom);
-
-		$s = $this->getLonLat($xtile, $ytile, $zoom);
-		$e = $this->getLonLat($xtile+1, $ytile+1, $zoom);
-
-		$s[0] = ($s[0]%360)+($s[0]-floor($s[0]));
-		$e[0] = ($e[0]%360)+($e[0]-floor($e[0]));
-
-		$s[0] = $s[0] >= 180? $s[0]-360:$s[0];
-		$e[0] = $e[0] > 180? $e[0]-360:$e[0];
-		 
-		$bbox = $s[0].",".$e[1].",".$e[0].",".$s[1];				
+		$bfile = $dir."/".$ytile;						
 		
-		shell_exec("python tilep.py -i ".$xml.".xml -o ".$bfile." -b ".$bbox);				
-
 		header("Access-Control-Allow-Origin: *");	
 		if ($type == 'utf')
 		{
 			header("Content-Type: text/json");
-			readfile($bfile.".json");	
+			$file = $bfile.".json";	
 		}
 		else
-		{	
+		{				
 			header("Content-Type: image/png");
-			readfile($img);
+			$file = $bfile.".png";
+		}				
+		
+		if (file_exists($file) && !$clear)
+		{			
+			readfile($file);
+		}
+		else
+		{						
+			$width = 256; 
+			$height = 256;
+			$tile_size = 256;
+
+			$xtile_s = ($xtile * $tile_size - $width/2) / $tile_size;
+			$ytile_s = ($ytile * $tile_size - $height/2) / $tile_size;
+			$xtile_e = ($xtile * $tile_size + $width/2) / $tile_size;
+			$ytile_e = ($ytile * $tile_size + $height/2) / $tile_size; 
+
+			$s0 = $this->getLonLat($xtile_s, $ytile_s, $zoom);
+			$e0 = $this->getLonLat($xtile_e, $ytile_e, $zoom);
+
+			$s = $this->getLonLat($xtile, $ytile, $zoom);
+			$e = $this->getLonLat($xtile+1, $ytile+1, $zoom);
+
+			$s[0] = ($s[0]%360)+($s[0]-floor($s[0]));
+			$e[0] = ($e[0]%360)+($e[0]-floor($e[0]));
+
+			$s[0] = $s[0] >= 180? $s[0]-360:$s[0];
+			$e[0] = $e[0] > 180? $e[0]-360:$e[0];
+			 
+			$bbox = $s[0].",".$e[1].",".$e[0].",".$s[1];				
+			
+			shell_exec("python tilep.py -i ".$xml.".xml -o ".$bfile." -b ".$bbox);				
+			readfile($file);
+		
 		}
 
 	}
